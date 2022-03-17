59e1069e71ef8fd5398b22d29c686d47dd410824
Casey Niehaus <cjniehaus.cn@gmail.com>
2022-03-16 20:03:36 -0400

    Added product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
 import { VendorListComponent } from './features/vendor-list/vendor-list.component';
 import { VendorDetailComponent } from './features/vendor-detail/vendor-detail.component';
 import { ProductListComponent } from './features/product-list/product-list.component';
+import { ProductDetailComponent } from './features/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ProductListComponent } from './features/product-list/product-list.compo
     UserDetailComponent,
     VendorListComponent,
     VendorDetailComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -9,8 +9,8 @@ export class Product {
   unit: string;
   photoPath: string;
 
-  constructor(id: number = 0, vendor: Vendor, partNumber: string = '',
-  name: string = '', price: number = 0.00, unit: string = '', photoPath: string = '') {
+  constructor(id: number = 0, vendor: Vendor = new Vendor(), partNumber: string = '',
+  name: string = '', price: number = 0, unit: string = '', photoPath: string = '') {
     this.id = id;
     this.vendor = vendor;
     this.partNumber = partNumber;
